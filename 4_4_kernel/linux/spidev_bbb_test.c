d7e0dc35e0bc622608d5b04bfbab888b8e7c196b
MaciekDudynski <maciekdudynski@gmail.com>
2017-11-22 17:07:21 +0100

    Test.

--- 4_4_kernel/linux/spidev_bbb_test.c
+++ 4_4_kernel/linux/spidev_bbb_test.c
@@ -33,154 +33,141 @@ static void pabort(const char *s)
 
 static uint32_t mode;
 static uint8_t bits = 8;
-static uint32_t speed = 100000;
+static uint32_t speed = 500000;
 static int verbose;
 
-uint8_t default_tx[3]= {
+uint8_t adc_tx[3]= {
 	0x06, 0x00, 0x00
 };
 
-uint8_t default_rx[3] = {
+uint8_t adc_rx[3] = {
 	0x00, 0x00, 0x00
 };
 
-static void hex_dump(const void *src, size_t length, size_t line_size,
-		     char *prefix)
+static void adc_hex_dump( const void *src, char *prefix )
 {
 	int i = 0;
 	const unsigned char *address = src;
-	const unsigned char *line = address;
-	unsigned char c;
 
-	printf("%s | ", prefix);
-	while (length-- > 0) {
-		printf("%02X ", *address++);
-		if (!(++i % line_size) || (length == 0 && i % line_size)) {
-			if (length == 0) {
-				while (i++ % line_size)
-					printf("__ ");
-			}
-			printf(" | ");  /* right close */
-			while (line < address) {
-				c = *line++;
-				printf("%c", (c < 33 || c == 255) ? 0x2E : c);
-			}
-			printf("\n");
-			if (length > 0)
-				printf("%s | ", prefix);
-		}
+	printf( "%s | ", prefix );
+	for( ; i < 3; ++i )
+	{
+		printf( "%02X ", *(address + i) );
 	}
+	printf( "| %4d\n", ( (short)(*(address + 1)) << 8 ) | (short)(*(address + 2)) );
 }
 
-static void transfer(int fd, uint8_t const *tx, uint8_t const *rx, size_t len)
+static void transfer_adc( int fd, uint8_t const *tx, uint8_t const *rx )
 {
 	int ret;
 	struct spi_ioc_transfer tr = {
 		.tx_buf = (unsigned long)tx,
 		.rx_buf = (unsigned long)rx,
-		.len = len,
+		.len = 3,
 		.delay_usecs = 0,
 		.speed_hz = speed,
 		.bits_per_word = bits,
 	};
 
-	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-	if (ret < 1)
-		pabort("can't send spi message");
+	ret = ioctl( fd, SPI_IOC_MESSAGE(1), &tr );
+	if( ret < 1 )
+		pabort( "can't send spi message" );
 
-	if (verbose)
+	if( verbose )
 	{
-		hex_dump(tx, len, 32, "TX");
-		hex_dump(rx, len, 32, "RX");
+		adc_hex_dump( tx, len, "ADC TX" );
+		adc_hex_dump( rx, len, "ADC RX" );
 	}		
 }
 
-static void print_usage(const char *prog)
+static void print_usage( const char *prog )
 {
-	printf("Usage: %s [-v]\n", prog);
-	puts("  -v --verbose  Verbose (show tx buffer)\n");
-	exit(1);
+	printf( "Usage: %s [-v]\n", prog );
+	puts( "  -v --verbose  Verbose (show tx buffer)\n" );
+	exit( 1 );
 }
 
-static void parse_opts(int argc, char *argv[])
+static void parse_opts( int argc, char *argv[] )
 {
-	while (1) {
+	while( 1 ) {
 		static const struct option lopts[] = {
 			{ "verbose", 0, 0, 'v' },
 			{ NULL, 0, 0, 0 },
 		};
 		int c;
 
-		c = getopt_long(argc, argv, "v",
-				lopts, NULL);
+		c = getopt_long( argc, argv, "v", lopts, NULL );
 
 		if( c == -1 )
 			break;
 
-		switch (c) {
-		case 'v':
-			verbose = 1;
-			break;
-		default:
-			print_usage(argv[0]);
-			break;
+		switch( c )
+		{
+			case 'v':
+				verbose = 1;
+				break;
+			default:
+				print_usage(argv[0]);
+				break;
 		}
 	}
 }
 
-int main(int argc, char *argv[])
+int main( int argc, char *argv[] )
 {
 	int ret = 0;
-	int fd;
+	int adc_fd;
 
-	parse_opts(argc, argv);
+	parse_opts( argc, argv );
 
-	fd = open("/dev/spidev1.0", O_RDWR);
-	if (fd < 0)
-		pabort("can't open device");
+	adc_fd = open( "/dev/spidev1.0", O_RDWR );
+	if( adc_fd < 0 )
+		pabort( "can't open device" );
 
 	/*
 	 * spi mode
 	 */
-	ret = ioctl(fd, SPI_IOC_WR_MODE32, &mode);
-	if (ret == -1)
-		pabort("can't set spi mode");
+	ret = ioctl( adc_fd, SPI_IOC_WR_MODE32, &mode );
+	if( ret == -1 )
+		pabort( "can't set spi mode" );
 
-	ret = ioctl(fd, SPI_IOC_RD_MODE32, &mode);
-	if (ret == -1)
-		pabort("can't get spi mode");
+	ret = ioctl( adc_fd, SPI_IOC_RD_MODE32, &mode );
+	if( ret == -1 )
+		pabort( "can't get spi mode" );
 
 	/*
 	 * bits per word
 	 */
-	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-	if (ret == -1)
-		pabort("can't set bits per word");
+	ret = ioctl( adc_fd, SPI_IOC_WR_BITS_PER_WORD, &bits );
+	if( ret == -1 )
+		pabort( "can't set bits per word" );
 
-	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
-	if (ret == -1)
-		pabort("can't get bits per word");
+	ret = ioctl( adc_fd, SPI_IOC_RD_BITS_PER_WORD, &bits );
+	if( ret == -1 )
+		pabort( "can't get bits per word" );
 
 	/*
 	 * max speed hz
 	 */
-	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-	if (ret == -1)
-		pabort("can't set max speed hz");
+	ret = ioctl( adc_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed );
+	if( ret == -1 )
+		pabort( "can't set max speed hz" );
 
-	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
-	if (ret == -1)
-		pabort("can't get max speed hz");
+	ret = ioctl( adc_fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed );
+	if( ret == -1 )
+		pabort( "can't get max speed hz" );
 
-	printf("spi mode: 0x%x\n", mode);
-	printf("bits per word: %d\n", bits);
-	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
+	printf( "spi mode: 0x%x\n", mode );
+	printf( "bits per word: %d\n", bits );
+	printf( "max speed: %d Hz (%d KHz)\n", speed, speed/1000 );
 	
 	uint16_t i = 0;
 	for( ; i < 10; ++i )
-		transfer( fd, default_tx, default_rx, sizeof(default_tx) );
+	{
+		transfer_adc( adc_fd, adc_tx, adc_rx );
+	}
 
-	close( fd );
+	close( adc_fd );
 
 	return ret;
 }
