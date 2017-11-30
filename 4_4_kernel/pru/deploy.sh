4c315ed577e37707e85b30e5df17ce4a3070cb4a
Maciej Dudyński <maciekdudynski@gmail.com>
2017-11-30 12:00:25 +0000

    4_4_kernel/pru/deploy.sh now runs make and supports -c (pru core)

--- 4_4_kernel/pru/deploy.sh
+++ 4_4_kernel/pru/deploy.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
 
-while getopts "p:" opt; do
-  case $opt in
-    p)
-      proj=${OPTARG}
-      ;;
-  esac
-done
-
 PRU_CORE=1
 
-echo "-Placing firmware"
-	cp ${proj}/gen/*.out /lib/firmware/am335x-pru${PRU_CORE}-fw
+while getopts "p:c:" opt; do
+	case $opt in
+		p)
+			PROJ=${OPTARG}
+			;;
+		c)
+			PRU_CORE=${OPTARG}
+			;;
+	esac
+done
 
-echo "-Rebooting"
+	make p=${PROJ}
+echo "************************************************************"
+echo "Placing firmware on PRU${PRU_CORE}"
+	cp ${PROJ}/gen/*.out /lib/firmware/am335x-pru${PRU_CORE}-fw
+echo "************************************************************"
+echo ""
+echo "************************************************************"
+echo "Rebooting PRU${PRU_CORE}"
 	if [ $PRU_CORE -eq 0 ]
 	then
 		echo "4a334000.pru0" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
@@ -22,3 +29,5 @@ echo "-Rebooting"
 		echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/unbind 2> /dev/null
 		echo "4a338000.pru1" > /sys/bus/platform/drivers/pru-rproc/bind
 	fi
+echo "************************************************************"
+echo ""
